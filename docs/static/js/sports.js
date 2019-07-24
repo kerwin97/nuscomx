794c6af7c46aaab4a8759e7facc2acca8f34a36c
Benjamin Lau <benjaminlau@Benjamins-MacBook-Pro.local>
2019-07-24 19:54:53 +0800

    Updated Sports Page

--- docs/static/js/sports.js
+++ docs/static/js/sports.js
@@ -11,7 +11,7 @@ function verifyCaptcha(response) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            window.open(this.responseText);
+            window.location.href(this.responseText);
         }
     };
     xhttp.open("POST", "https://api.nuscomputing.com/index.php", true);
