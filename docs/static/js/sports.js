181be7fab6ac73d41d29bc0b9fb691e4ad0b89ab
Benjamin Lau <benjaminlau@Benjamins-MacBook-Pro.local>
2019-07-24 20:16:12 +0800

    Updated Sports Page

--- docs/static/js/sports.js
+++ docs/static/js/sports.js
@@ -7,27 +7,16 @@ function hideCaptchas() {
 }
 function verifyCaptcha(response) {
     console.log(response);
-    alert('1');
     var xhttp = new XMLHttpRequest();
-    alert('2');
     xhttp.onreadystatechange = function() {
-    	alert('A');
         if (this.readyState == 4 && this.status == 200) {
-    		alert('B');
         	document.body.innerHTML = "<a href='" + this.responseText + "'>Click here if you are not redirected</a>";
-   			alert('C');
-        	alert("You will be redirected shortly!");
             window.location.href(this.responseText);
-    		alert('D');
         }
     };
-    alert('3');
     xhttp.open("POST", "https://api.nuscomputing.com/index.php", true);
-    alert('4');
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    alert('5');
     xhttp.send("id=" + selected + "&resp=" + response);
-    alert('6');
 }
 function handleEvent(passedInElement) {
     return function(e) {
